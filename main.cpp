6b47bd7084f9b0c6645f1bf16caa2e8e17a9ee81
emese-molnar <emolnar04@gmail.com>
2026-02-27 11:48:36 +0100

    Hibák javítása

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //N_ELEMENTS a NELEMENTS helyett
-    std::cout << '1-100 ertekek duplazasa' //helyette: std::cout << "1-100 ertekek duplazasa" << std::endl;
-    for (int i = 0;) //helyette: for (int i = 0; i < N_ELEMENTS; i++)
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2; //(i+1) * 2
+        b[i] = (i+1) * 2;
     }
-    for (int i = 0; i; i++) //helyette: for (int i = 0; i < N_ELEMENTS; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" //hiányzik:<< b[i << std::endl;
+        std::cout << "Ertek:" << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //double int helyett
-    for (int i = 0; i < N_ELEMENTS, i++) //i++ elé pontosvessző kell
+    double atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] //hiányzik a ;
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    //memória felszabadítása: delete[] b;
+    delete[] b;
     return 0;
 }
